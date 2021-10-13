858d4950861a2876ec098eb5dfde5218f1a1d3f0
AaryamanBhute <aryamanbhute@gmail.com>
2021-10-13 16:26:25 -0700

    removed excess static files

--- staticfiles/website/loadHeader.926dc804c234.js
+++
@@ -1,8 +0,0 @@
-var app = document.getElementById('header');
-
-var typewriter = new Typewriter(app, {
-    loop: false,
-    cursor: "",
-    delay: 50
-});
-typewriter.typeString("<strong><span>&#8212;</span> Hey, I'm Aaryaman <span>&#8212;</span></strong>").start().pauseFor(2500)
