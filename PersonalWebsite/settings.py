89d36145977018feccc99d3a759a6a0aba8bb0e5
AaryamanBhute <aryamanbhute@gmail.com>
2021-10-13 16:25:14 -0700

    update

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#kj8hi8hdpgtc$f65_ca7=opwsx=yt(#5)#po*s9vru1=&x4xm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']   
 
